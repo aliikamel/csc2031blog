044f2650e9f33349efa2b7737d603cbe260553aa
c1034102 <c1034102@ncl.ac.uk>
2022-10-04 14:49:20 +0100

    updated register.html and views.py

--- users/views.py
+++ users/views.py
@@ -4,9 +4,16 @@ from users.forms import RegisterForm
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
 
-@users_blueprint.route('/register')
+@users_blueprint.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('users/register.html')
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        print(request.form.get('username'))
+        print(request.form.get('password'))
+        return redirect(url_for('users.login'))
+
+    return render_template('users/register.html', form=form)
 
 
 @users_blueprint.route('/login')
@@ -19,14 +26,5 @@ def logout():
     return render_template('users/logout.html')
 
 
-@users_blueprint.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
 
-    if form.validate_on_submit():
-        print(request.form.get('username'))
-        print(request.form.get('password'))
-        return redirect(url_for('users.login'))
-
-    return render_template('users/register.html', form=form)
 
