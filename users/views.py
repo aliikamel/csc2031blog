a36664340f76db1a9736344b0cac4cc98e2bf759
c1034102 <c1034102@ncl.ac.uk>
2022-10-04 14:31:33 +0100

    create forms.py and add instance of register form to users.views.py

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from users.forms import RegisterForm
 
 users_blueprint = Blueprint('users', __name__, template_folder='templates')
 
@@ -16,3 +17,16 @@ def login():
 @users_blueprint.route('/logout')
 def logout():
     return render_template('users/logout.html')
+
+
+@users_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        print(request.form.get('username'))
+        print(request.form.get('password'))
+        return redirect(url_for('users.login'))
+
+    return render_template('users/register.html', form=form)
+
