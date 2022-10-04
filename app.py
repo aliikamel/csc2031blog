e067471604d0076e89b535d0f72d4c2e14e8138a
c1034102 <c1034102@ncl.ac.uk>
2022-10-04 14:38:22 +0100

    add secret token to apps.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 from main.views import main_blueprint
 from blog.views import blog_blueprint
 from users.views import users_blueprint
+import secrets
 
 
 app = Flask(__name__)
+app.config['Secret Key'] = secrets.token_hex(16)
 
 app.register_blueprint(main_blueprint)
 app.register_blueprint(blog_blueprint)
