868d3318cd7e80f7d0fc8867e668afbed207a361
Big Chamel <ali10.imkamel@gmail.com>
2022-10-04 19:25:28 +0100

    implement error handling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from main.views import main_blueprint
 from blog.views import blog_blueprint
 from users.views import users_blueprint
@@ -11,6 +11,22 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.register_blueprint(main_blueprint)
 app.register_blueprint(blog_blueprint)
 app.register_blueprint(users_blueprint)
+
+
+@app.errorhandler(403)
+def function_name(error):
+    return render_template('403.html'), 403
+
+
+@app.errorhandler(404)
+def function_name(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def function_name(error):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     app.run()
