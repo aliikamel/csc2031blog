97219b0fa8c7f32f81f3036a2175e0f1b808a776
Big Chamel <ali10.imkamel@gmail.com>
2022-10-11 21:44:06 +0100

    updates

--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from app import db
 
 
@@ -7,7 +9,25 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(length=100), nullable=False, unique=True)
     password = db.Column(db.String(length=100), nullable=False)
+    blogs = db.relationship('Post')
 
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(length=100), db.ForeignKey(User.username), nullable=True)
+    created = db.Column(db.Datetime)
+    title = db.Column(db.Text, nullable=False)
+    body = db.Column(db.Text, nullable=False)
+
+    def __init__(self, username, body, title):
+        self.username = username
+        self.created = datetime.now()
+        self.body = body
+        self.title = title
+
